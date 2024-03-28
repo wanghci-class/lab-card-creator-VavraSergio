101e9ff242d81fb309bbba4116e970222ae44dba
your_username <sergio.vavra@gmail.com>
2024-03-28 15:09:24 -0400

    Deletable cards is done.

--- scripts/viewcards.js
+++ scripts/viewcards.js
@@ -10,7 +10,15 @@ for (let i = 0; i < cards.length; i++) {
 
     let cardView = template.content.cloneNode(true);
 
+    let titleText = cardView.querySelector(".title-text")
+    titleText.textContent = card.title;
 
+    let deleteBtn = cardView.querySelector(".delete-btn");
+    deleteBtn.addEventListener("click", function () {
+        cards.splice(i, 1);
+        localStorage.setItem("cards", JSON.stringify(cards));
+        location.reload();
+    });
 
     cardList.appendChild(cardView);
 }
